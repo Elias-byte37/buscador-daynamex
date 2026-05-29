d11c21f2d9c2ab8391bff4edadc5e28f9a3dd14e
Elias-byte37 <dynamexmarketing@gmail.com>
2026-05-29 12:40:52 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,10 @@ import io
 
 st.set_page_config(page_title="Buscador Daynamex", layout="centered")
 
-# --- PEGA AQUÍ LOS DOS ENLACES CSV (uno por pestaña) ---
-URL_1 = https://docs.google.com/spreadsheets/d/e/2PACX-1vSNPiCLWRdXv7mE3SSoIxh055sOftk2JzVOxrNBulDzbzqooFV2erznw-9HpyEIBhtASghBxZcFTSvX/pub?gid=68779616&single=true&output=csv
-URL_2 = https://docs.google.com/spreadsheets/d/e/2PACX-1vSNPiCLWRdXv7mE3SSoIxh055sOftk2JzVOxrNBulDzbzqooFV2erznw-9HpyEIBhtASghBxZcFTSvX/pub?gid=707680383&single=true&output=csv
+# --- URLs corregidas con comillas ---
+URL_1 = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSNPiCLWRdXv7mE3SSoIxh055sOftk2JzVOxrNBulDzbzqooFV2erznw-9HpyEIBhtASghBxZcFTSvX/pub?gid=68779616&single=true&output=csv"
+URL_2 = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSNPiCLWRdXv7mE3SSoIxh055sOftk2JzVOxrNBulDzbzqooFV2erznw-9HpyEIBhtASghBxZcFTSvX/pub?gid=707680383&single=true&output=csv"
+
 @st.cache_data(ttl=60)
 def get_data(url):
     try:
